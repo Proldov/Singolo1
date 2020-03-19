091ff6d92786c963dac0be4585b491c580708f9d
Proldov <58115549+Proldov@users.noreply.github.com>
2020-03-19 14:15:06 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,35 +1,4 @@
 
-// window.onload = function() {
-   
-//     //navigation
-//     AddNavigationClicHendler();
-// }
-
-// const AddNavigationClicHendler = () => {
-//     document.querySelector('.navigation').addEventListener('click', (e) => {
-//         console.log(e); 
-//         if (e.target.classList.contains('navigation__link')) {
-//             let clickedNav = e.target; // тот элемент на котором произошло событие
-//             removeSelectedNav();
-//             selectClickedNav(clickedNav);
-//         }  
-//     })
-// }
-
-// const removeSelectedNav = () => {
-//     let navigationLink = document.querySelectorAll('.navigation, .navigation__link');
-//     navigationLink.forEach(navigation__link => {
-//         navigation__link.classList.remove('navigation__link_bordered');
-//         navigation__link.classList.add('navigation__link_selected');
-//     })
-    
-// }
-
-// const selectClickedNav = (clickedNav) => {
-//     clickedNav.classList.add('navigation__link_bordered');
-//     clickedNav.classList.remove('navigation__link_selected');
-// };
-
 
 //slaider
 
@@ -74,67 +43,6 @@
     });
 
 
-//     // off phone
-// //     window.onload = function() {    
-// //     AddPhoneClicHendler();
-// // }
-// //     const AddPhoneClicHendler = () => {
-// //         document.querySelector('.slide').addEventListener('click', (e) => {
-// //             console.log(e); 
-// //             if (e.target.classList.contains('.bcg_vertical')) {
-// //                 let clickedPhone = e.target; // тот элемент на котором произошло событие
-// //                 removeSelectedNav();
-// //                 selectClickedNav(clickedPhone);
-// //             }  
-// //         })
-// //     }
-
-// //     const removeSelectedPhone = () => {
-// //         let bcgVertical = document.querySelectorAll('.slide, .bcg_vertical');
-// //         bcgVertical.forEach(bcg_vertical => {
-// //             bcg_vertical.classList.remove('dispOff');
-// //             bcg_vertical.classList.add('dispOff');
-// //         })
-
-// let phone = 1,
-//     picture = document.getElementsByClassName('bcg_vertical');
-//     // left = document.querySelector('.promo__arrow-left'),
-//     // right = document.querySelector('.promo__arrow-right'); 
-
-//      showPhone(phone);
-
-
-//     function showPhone(n)  {
-//         if (n > picture.length) {
-//             phone = 1;
-//         };
-
-//         if (n < 1) {
-//             phone = picture.length;
-//         };
-
-//         for (let i = 0 ; i < slides.length; i++) { // скрываем слайды
-//            picture[i].style.display ='none';                                               
-//         };
-
-//         picture[phone - 1].style.display ='block'; 
-//     };    
-
-//     function plusPhone(n) {
-//         showPhone(phone+=n);
-//     }
-
-//     left.addEventListener('click', function(){
-//         plusPhone(-1);    
-//         slides[slideIndex - 1].classList.remove('.promo__arrow-left');  
-//         slides[slideIndex - 1].classList.add('.promo__arrow-right');
-//     })
-
-//     right.addEventListener('click', function(){
-//         plusSlides(1);
-//         slides[slideIndex - 1].classList.remove('.promo__arrow-right');  
-//         slides[slideIndex - 1].classList.add('.promo__arrow-left');      
-//     });
 
 //menu
 
@@ -164,22 +72,17 @@ document.getElementsByClassName('bcg_horizontal')[0].addEventListener('click', p
 
 //Portfolio
 
-// const AddPortfolioClicHendler = document.getElementById('portfolio__tags');
-// AddPortfolioClicHendler.addEventListener('click', function(event){
-//     const tablistBord = AddPortfolioClicHendler.querySelectorAll('.layout-tags__tag');
-//     for (let i = 0;i < tablistBord.length;i++) {
-//         tablistBord[i].classList.remove('layout-tags_bordered');    
-//     };
-//     event.target.classList.add('layout-tags_bordered');
+const AddPortfolioClicHendler = document.getElementById('portfolio__tags');
+AddPortfolioClicHendler.addEventListener('click', function(event){
+    const tablistBord = AddPortfolioClicHendler.querySelectorAll('.layout-tags__tag');
+    for (let i = 0;i < tablistBord.length;i++) {
+        tablistBord[i].classList.remove('layout-tags_bordered');    
+    };
+    event.target.classList.add('layout-tags_bordered');
     
 
-// });
+});
 
-// const potfolioImage = document.getElementById('potfolio__image');
-// potfolioImage.addEventListener ('',function () {
-//     let a = potfolioImage.querySelectorAll('.layoyt-4-column__image');   
-//     console.log(a)
-// });
 
 let a = document.querySelectorAll('.layoyt-4-column__image');
 let count = 0;
@@ -190,6 +93,25 @@ function compareRandom() {
 };
     a.sort(compareRandom);
     console.log(a);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     // function shuffle() {
     //     return a.sort(function() {return 0.5 - Math.random()});
