69152272bf763f6cefa2aa5b28a003c334099452
Proldov <58115549+Proldov@users.noreply.github.com>
2020-03-21 15:21:01 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -52,8 +52,52 @@ MENU.addEventListener('click', function(event){
     for (let i = 0; i < menuList.length; i++) {
        menuList[i].classList.remove('navigation__link_bordered');
     }
-    event.target.classList.add('navigation__link_bordered');
+    event.target.classList.add('navigation__link_bordered');    
 });
+
+//Обработка скрола
+  
+let avgHeight = 0;
+const SECTIONs = document.querySelectorAll('section');
+SECTIONs.forEach(el => {
+    avgHeight += el.offsetHeight; 
+});
+
+let ScrollOffset = document.documentElement.clientHeight - parseInt(avgHeight / SECTIONs.length);
+ScrollOffset = (ScrollOffset < 30) ? document.querySelector('header').offsetHeight : ScrollOffset;
+
+document.addEventListener('scroll', event => {
+    let curPos = window.scrollY + ScrollOffset;
+    const elList = document.querySelectorAll('section');
+    console.log(elList)
+  
+    const menuList =  MENU.querySelectorAll('.navigation__link');
+    console.log( menuList)
+    elList.forEach(el => {
+        if ((el.offsetTop) <= curPos && (el.offsetTop + el.offsetHeight - 60) > curPos) {
+            menuList.forEach(li => {
+                li.classList.remove('navigation__link_bordered');
+                if (el.getAttribute('id') === li.querySelector('a').getAttribute('href').substring(1)) {
+                    li.classList.add('navigation__link_bordered');
+                }
+            });
+        }
+    });
+ 
+
+});
+
+
+
+
+// document.addEventLissener('scroll', function scrollHandler (){
+
+// })
+
+// function scrollHandler() {
+//     let menu =document.querySelector('header');
+
+// };
 
 //phoneOff
 
@@ -71,7 +115,7 @@ document.getElementById('phone__hor').addEventListener('click', phonesHorizontal
 document.getElementsByClassName('bcg_horizontal')[0].addEventListener('click', phonesHorizontal);
 
 //Portfolio
-
+//табы
 const AddPortfolioClicHendler = document.getElementById('portfolio__tags');
 AddPortfolioClicHendler.addEventListener('click', function(event){
     const tablistBord = AddPortfolioClicHendler.querySelectorAll('.layout-tags__tag');
@@ -79,31 +123,49 @@ AddPortfolioClicHendler.addEventListener('click', function(event){
         tablistBord[i].classList.remove('layout-tags_bordered');    
     };
     event.target.classList.add('layout-tags_bordered');
-    
 
+    //цикл для переммешивания картинок
+
+    let a = document.querySelectorAll('.layoyt-4-column__image');
+    let arr = [].slice.call(a)   //возводим a в мвссив
+    function compareRandom() {
+        
+        return Math.random() - 0.5;    
+    };
+       arr.sort(compareRandom);
+       
+        for (let i = 0; i < arr.length; i++) {
+            document.querySelector('#potfolio__image').append(arr[i]);
+        };    
 });
 
+//рамка
 
-let a = document.querySelectorAll('.layoyt-4-column__image');
-let count = 0;
-function compareRandom() {
-    count++;
-    return Math.random() - 0.5;
+const borderImg = document.getElementById('potfolio__image');
+borderImg.addEventListener('click', function(event){
+    const borderList = borderImg.querySelectorAll('.layoyt-4-column__image'); 
+    for (let i = 0;i < borderList.length;i++) {
+        borderList[i].classList.remove('portfolio__bordered');       
+    };
+    event.target.classList.add('portfolio__bordered');
+   
+});   
+
+
+// let a = document.querySelectorAll('.layoyt-4-column__image');
+// let arr = [].slice.call(a)
+
+
+// function compareRandom() {
+    
+//     return Math.random() - 0.5;
   
-};
-    a.sort(compareRandom);
-    console.log(a);
-
-
-
-
-
-
-
-
-
-
-
+// };
+//    arr.sort(compareRandom);
+   
+//     for (let i = 0; i < arr.length; i++) {
+//         document.querySelector('#potfolio__image').append(arr[i]);
+//     };
 
 
 
@@ -115,6 +177,27 @@ function compareRandom() {
 
     // function shuffle() {
     //     return a.sort(function() {return 0.5 - Math.random()});
+
+
+
+// function shuffle() {
+//         return arr.sort(function() {return 0.5 - Math.random()});
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
         
     // };
     // console.log(shuffle);
